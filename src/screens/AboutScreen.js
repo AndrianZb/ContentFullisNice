fb8a8a3e9e34a3dcd2e9870ed4e51e8b6d3295fa
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-10 22:32:23 -0400

    Update AboutScreen.js

---
+++ src/screens/AboutScreen.js
@@ -0,0 +1,36 @@
+const AboutScreen = {
+  render: () => {
+    return `<div class="blog-detail">
+            <div class="side-ad">
+              <a href="#" class="vertical-ad">
+              <img src="/images/side-ad.svg"/>
+              </a>
+            </div>
+            <div class="article-container">
+            <div class="content">
+            <div class="top">
+            <h1 id="AboutMe"></h1>
+            </div>
+            <p>
+           !!!
+            </p>
+            <div class="content-img">
+            <img src="./images/profile.jpg" alt="" />
+            </div>
+            <p>
+            weawe
+            </p>
+            <p>
+          assss
+            </p>
+            </div>
+            </div>
+            <div class="side-ad">
+              <a href="#" class="vertical-ad">
+              <img src="/images/side-ad.svg"/>
+              </a>
+            </div>
+            </div>`;
+  },
+};
+export default AboutScreen;
