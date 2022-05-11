a02790f93334a54e9671443ed2f55cdc893c8746
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-10 23:10:47 -0400

    d

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const navList = document.querySelector(".nav-list");
+/*const navList = document.querySelector(".nav-list");
 const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector(".header");
 
@@ -15,3 +15,4 @@ window.addEventListener("scroll", () => {
     header.classList.remove("fix");
   }
 });
+*/
