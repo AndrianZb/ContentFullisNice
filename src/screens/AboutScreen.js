a93d91a72250c76266c9e2b820a751a3b4a54759
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-14 17:33:07 -0400

    f
    f

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -13,9 +13,11 @@ const AboutScreen = {
             <div class="article-container">
             <div class="content">
             <div class="top">
-            <h1 id="AboutMe">!!hgee!</h1>
+            <h1 id="AboutMe">Andrian Zberea</h1>
             </div>
+            <h2>Web Developer, Software Developer & Machine Learning Enthusiast.            </h2>
             <p>
+            Proven technical abilities and a distinguished record for achieving cost-saving measures in a secure environment. Highly energetic and driven to succeed. Recognized for interpersonal and problem-solving skills.
             </p>
             <div class="content-img">
             <img src="../images/profile1.jpg" alt="heyy" />
