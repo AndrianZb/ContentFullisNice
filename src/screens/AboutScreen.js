096a82d2e88dc26676450c1f77c02d02586ffcc2
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-14 16:38:09 -0400

    g
    g

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -6,8 +6,8 @@ import { getBlogs } from "../api.js";
 const AboutScreen = {
   
   render: async () => {
-    const blogs = await getBlogs();
-    alert(blogs);
+    //const blogs = await getBlogs();
+  //  alert(blogs);
     return `<div class="blog-detail">
            
             <div class="article-container">
@@ -18,7 +18,7 @@ const AboutScreen = {
             <p>
             </p>
             <div class="content-img">
-            <img src="./images/profpicture2.jpg" alt="" />
+            <img src="./images/profpicture2.jpg" alt="hey" />
             </div>
             <p>
             weawe
