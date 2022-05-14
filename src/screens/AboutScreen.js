3e0295091db3181b2a0d7b0b68ecb3b1686c4202
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-14 11:08:09 -0400

    ball
    123

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,6 +1,20 @@
 import { getBlogs } from "../api.js";
 import {str} from "../api.js";
 
+const client = require('contentful').createClient({
+  space: "sqzdd6o0iz0g",
+  accessToken: "x9jae8sbU24uh4UX_IpBp9Y8Tg2KT2FEe7M-ngM6pqk",
+})
+
+export const getStaticProps = async () => {
+  const data = await client.getEntries();
+  console.log("hello you" + data);
+
+
+}
+
+
+
 const AboutScreen = {
   
   render: () => {
