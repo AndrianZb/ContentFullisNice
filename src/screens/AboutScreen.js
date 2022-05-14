5c742ec7c52053e245933df0bf3221692388453e
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-14 11:29:11 -0400

    f
    f

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -19,11 +19,7 @@ const AboutScreen = {
   
   render: () => {
     return `<div class="blog-detail">
-            <div class="side-ad">
-              <a href="#" class="vertical-ad">
-              <img src="/images/side-ad.svg"/>
-              </a>
-            </div>
+           
             <div class="article-container">
             <div class="content">
             <div class="top">
