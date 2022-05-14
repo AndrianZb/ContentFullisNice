3e95dc391ad80a24746b6a7165d22bde63987014
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-14 12:21:21 -0400

    Update AboutScreen.js
    f

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -7,7 +7,7 @@ const AboutScreen = {
   
   render: async () => {
     const blogs = await getBlogs();
-
+    alert(blogs);
     return `<div class="blog-detail">
            
             <div class="article-container">
