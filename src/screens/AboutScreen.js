0926bd81a5e66294f45e5589d5ece1e66634534a
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-14 11:37:22 -0400

    v
    v

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,29 +1,18 @@
 import { getBlogs } from "../api.js";
 import {str} from "../api.js";
 
-const client = require('contentful').createClient({
-  space: "sqzdd6o0iz0g",
-  accessToken: "x9jae8sbU24uh4UX_IpBp9Y8Tg2KT2FEe7M-ngM6pqk",
-})
-
-export const getStaticProps = async () => {
-  const data = await client.getEntries();
-  alert("hello you" + data);
-
-
-}
-
-
 
 const AboutScreen = {
   
-  render: () => {
+  render: async () => {
+    const blogs = await getBlogs();
+
     return `<div class="blog-detail">
            
             <div class="article-container">
             <div class="content">
             <div class="top">
-            <h1 id="AboutMe">${getStaticProps.data}</h1>
+            <h1 id="AboutMe">${str}</h1>
             </div>
             <p>
             </p>
