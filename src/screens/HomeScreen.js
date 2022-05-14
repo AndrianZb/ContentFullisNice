6bb6b54811e1b2565db9b7bb75f1755851d91f83
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-14 19:21:44 -0400

    m

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import { getBlogs } from "../api.js";
 
 import Blog from "../components/Blog.js";
-
+/*
 const HomeScreen = {
   render: async () => {
     const blogs = await getBlogs();
@@ -33,5 +33,14 @@ const HomeScreen = {
     }
   },
 };
+*/
 
+
+const HomeScreen = {
+  render: async () => {
+`<div> hello </div>`
+
+
+  },
+};
 export default HomeScreen;
