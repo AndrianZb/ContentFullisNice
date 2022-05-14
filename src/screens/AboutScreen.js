56bdbe9860c7aca0f852852c3b557a5be4fd6cd5
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-14 10:46:19 -0400

    Update AboutScreen.js
    ff

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,5 +1,5 @@
 import { getBlogs } from "../api.js";
-import {str2} from "../api.js";
+import {strValue} from "../api.js";
 
 const AboutScreen = {
   
@@ -13,7 +13,7 @@ const AboutScreen = {
             <div class="article-container">
             <div class="content">
             <div class="top">
-            <h1 id="AboutMe">${getBlogs.strValue}</h1>
+            <h1 id="AboutMe">${strValue}</h1>
             </div>
             <p>
             </p>
