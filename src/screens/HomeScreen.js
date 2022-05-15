fc3abd242d267002002c6bddf20de15fbafd9dfd
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-15 15:27:15 -0400

    c
    c

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,23 +6,16 @@ const HomeScreen = {
   render: async () => {
   
     return `<div>
-    <h1> Welcome to my e-portfolio. As a programmer, I am always open to learning new ideas and new technologies.<h1>
+    <h1> Welcome to my e-portfolio. As a programmer, I am always open to learning new ideas and new technologies.</h1>
 
-    <div class="content-img">
-    <img src="../images/wow.png" alt="heyy" />
-    <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
-
-
-    <div>As of late, my hobby is exploring machine learning methods.</div>
-    Please contact me in the <strong>Hire Me</strong> section for any inquiries.
-    </div>
-    
-    <div>My hobbies</div>
-    <div>Attending Cisco Connect Conventions</div>
-    <div>Going to Montreal theatres</div> 
-    <div>Collecting suits and watches</div>
-    </div>
-    
+    <div>
+    <p style="float: left;"><img src="http://placekitten.com/g/200/200" height="200px" width="200px" border="1px"></p>
+    <p>Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text</p>
+</div>
+<div style="clear: left;">
+    <p style="float: left;"><img src="http://placekitten.com/g/200/200" height="200" width="200" border="1px"></p>
+    <p>Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text</p>
+</div>
 
     <h2>Follow Contact Me On :    <a>GitHub</a>  <a>LinkedIn</a> </h2>
         
