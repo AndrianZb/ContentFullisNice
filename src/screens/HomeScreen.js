efed7e2c0dc99e913477b68fdb040d283153b7fe
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-15 09:42:18 -0400

    fg
    gf

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,7 +7,9 @@ const HomeScreen = {
   
     return `<div>
     <div class="blog-layout container" id="blog-layout">
-    hello
+    <h1> Welcome to my e-portfolio. As a programmer, I am always open to learning new ideas and new technologies.<h1?
+    <h2>As of late, my hobby is exploring machine learning methods.
+    Please contact me in the <strong>Hire Me</strong> section for any inquiries.<h2>
     </div>
     <div class="load-btn"><button class="load" id="load">Load more</button></div>
     </div>`;
