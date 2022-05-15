702ead825dc615338b5c52c18b53c5c7ef247baa
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-15 09:43:55 -0400

    f
    f

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,12 +6,9 @@ const HomeScreen = {
   render: async () => {
   
     return `<div>
-    <div class="blog-layout container" id="blog-layout">
-    <h1> Welcome to my e-portfolio. As a programmer, I am always open to learning new ideas and new technologies.<h1?
+    <h1> Welcome to my e-portfolio. As a programmer, I am always open to learning new ideas and new technologies.<h1>
     <h2>As of late, my hobby is exploring machine learning methods.
     Please contact me in the <strong>Hire Me</strong> section for any inquiries.<h2>
-    </div>
-    <div class="load-btn"><button class="load" id="load">Load more</button></div>
     </div>`;
   },
 };
