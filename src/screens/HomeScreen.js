4906b6b466e71d377c2716a982ebc454da2de071
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-15 19:19:06 -0400

    f
    f

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,7 +8,7 @@ const HomeScreen = {
     return `<div>
     <h1><i> Welcome to my e-portfolio. As a programmer, I am always open to learning new ideas and new technologies. </i></h1>
 <br/>
-    <div style="border-style: solid; border-color: #3890af; border-width: 50px" class="border border-primary">
+    <div style="border-style: solid; border-color: #fcfafa; border-width: 50px" class="border border-primary">
     <p style="float: left;"><img src="../images/wow.png" alt="heyy" /></p>
     <p>
     <h2 style="border-radius: 10px;background-color: lightblue;"> Welcome to my Portfolio ! </h2>
@@ -30,21 +30,11 @@ const HomeScreen = {
 </div>
 
 
-<div style="border-style: solid; border-color: #3890af; border-width: 50px" class="border border-primary">
+<div style="border-style: solid; border-color: #262932; border-width: 50px" class="border border-primary">
 <p style="float: right;"><img src="../images/watch.jpg" alt="heyy" /></p>
-<p>
-<h2 style="border-radius: 10px;background-color: lightblue;"> Welcome to my Portfolio ! </h2>
-<ul>
-<li>DEC, COMPUTER SCIENCE: TECHNOLOGY <strong>2019 - 2022</strong> <br/>Champlain Regional College</li>
-<li>CS50 PYTHON WEBINAR <strong>2021</strong> <br/>Harvard Online Course</li>
 
-</ul>  
-<h2 style="border-radius: 10px;background-color: lightblue;"> My hobbies are</h2>
-<ul>
-<li>Tweaking and collecting watches</li>
-<li>Learning about machine learning</li>
-<li></li>
-</ul>  </p>
+
+
 </div>
 
     <h2>Follow Me On :    <a>GitHub</a>  <a>LinkedIn</a> </h2>
