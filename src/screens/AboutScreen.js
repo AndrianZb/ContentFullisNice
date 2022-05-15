f7f5af7e6992e7d04dc1fd573f427b59203c62ad
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-15 13:16:44 -0400

    c
    c

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -16,22 +16,25 @@ const AboutScreen = {
 
             </div>
             <br/>
+            <hr/>
+            <br/>
             <h2>Web Developer, Software Developer & Machine Learning Enthusiast.</h2>
             <p>
-            Proven technical abilities and a distinguished record for achieving cost-saving measures in a secure environment. Highly energetic and driven to succeed. Recognized for good workplace ethics and problem-solving skills.
+            Proven technical abilities and a distinguished record for achieving cost-saving measures in a secure environment. Highly energetic and driven to succeed. 
+            <br/> Recognized for good workplace ethics and problem-solving skills.
             </p>
             </div>
             <h2>Education</h2>
 
             <ul>
-              <li>DEC, COMPUTER SCIENCE: TECHNOLOGY <strong>2019 - 2022</strong></li>
-              <li>CS50 PYTHON WEBINAR <strong>2021</strong></li>
+              <li>DEC, COMPUTER SCIENCE: TECHNOLOGY <strong>2019 - 2022</strong> Champlain Regional College</li>
+              <li>CS50 PYTHON WEBINAR <strong>2021</strong> Hardvard Online Course</li>
            
             </ul>  
             <h2>Professional Experience</h2>
             <ul>
-            <li>JUNIOR DEVELOPER</li>
-            <li>IT SUPPORT AGENT</li>
+            <li>JUNIOR DEVELOPER <strong>2022 - PRESENT</strong> NexOne, Saint-Hubert, QC</li>
+            <li>IT SUPPORT AGENT <strong>2019 - 2020</strong> NexOne, Saint-Hubert, QC</li>
             </ul>  
      
             </div>
