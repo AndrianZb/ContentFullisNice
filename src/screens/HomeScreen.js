587d7a24b6b9f55b86f9f900488e405e7a844eac
Andrian Z <70290099+AndrianZb@users.noreply.github.com>
2022-05-16 18:58:16 -0400

    n
    n

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,8 +6,22 @@ const HomeScreen = {
   render: async () => {
   
     return `<div>
-    <h1><i> Welcome to my e-portfolio. As a programmer, I am always open to learning new ideas and new technologies. </i></h1>
-<br/>
+   
+
+
+
+    <h2>Follow Me On :    <a>GitHub</a>  <a>LinkedIn</a> </h2>
+    
+    
+</div>
+    
+    `;
+  },
+};
+
+
+/* <h1><i> Welcome to my e-portfolio. As a programmer, I am always open to learning new ideas and new technologies. </i></h1>
+    <br/>
     <div style="border-style: solid; border-color: #fcfafa; border-width: 50px" class="border border-primary">
     <p style="float: left;"><img src="../images/wow.png" alt="heyy" /></p>
     <p>
@@ -27,19 +41,5 @@ const HomeScreen = {
 
 <div style="clear: left;">
    
-</div>
-
-
-
-    <h2>Follow Me On :    <a>GitHub</a>  <a>LinkedIn</a> </h2>
-    
-    
-</div>
-    
-    `;
-  },
-};
-
-
-
+</div>*/
 export default HomeScreen;
